eae6a2478b9119d1a00c7f2a2ad1b87db3d466cf
kkonfederatova <kseniya.konfederatova@gmail.com>
2025-01-10 17:09:22 +0200

    add lesson "Strings"

---
+++ lessons/strings/strings.js
@@ -0,0 +1,13 @@
+const makeCensored = (text, stopWords) => {
+  const words = text.split(' ');
+
+  const result = [];
+  for (const word of words) {
+    const newWord = stopWords.includes(word) ? '$#%!' : word;
+    result.push(newWord);
+  }
+
+  return result.join(' ');
+};
+
+export default makeCensored;
