fe2038c8115ed739db719d68ddbbccc5789704e9
kkonfederatova <kseniya.konfederatova@gmail.com>
2025-01-16 13:19:40 +0200

    add lesson "Spread operator"

---
+++ lessons/spread-operator/arrays.js
@@ -0,0 +1,12 @@
+export const flatten = (coll) => {
+  let result = [];
+  for (const item of coll) {
+    if (Array.isArray(item)) {
+      result = [...result, ...item];
+    } else {
+      result = [...result, item];
+    }
+  }
+
+  return result;
+};
